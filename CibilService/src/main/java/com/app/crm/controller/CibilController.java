7ee9041ecf5e3afe9b7b70d220ebb394906a83fd
HP <HP@DESKTOP-OB5TTT4>
2025-05-21 11:04:31 +0530

    CIBIL SERVCIE PUSH BY AKSHAY

---
+++ CibilService/src/main/java/com/app/crm/controller/CibilController.java
@@ -0,0 +1,32 @@
+package com.app.crm.controller;
+
+import java.util.Random;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CibilController {
+
+
+	
+	@GetMapping("/generateCibil")
+	public ResponseEntity<Integer> onSaveCustomerDetails(){
+		
+		
+		Random random=new  Random();
+	 int cibil=	random.nextInt(601)+300;
+		
+	
+	
+		return new ResponseEntity<Integer>(cibil,HttpStatus.CREATED);
+		
+		
+	
+	}
+
+	
+	
+}
